407775cacbdf33c4bc1cce864521644a22ded9e8
Jonathon <Jonatan9102@gmail.com>
2021-07-13 10:13:20 -0400

    Began filling in the skeleton of Receipt class

--- src/main/java/com/codediff/ocr/api/CashFlowManagementAPI/model/Receipt.java
+++ src/main/java/com/codediff/ocr/api/CashFlowManagementAPI/model/Receipt.java
@@ -1,4 +1,53 @@
 package com.codediff.ocr.api.CashFlowManagementAPI.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Receipt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    //List<Item> items;
+    Double totalPrice;
+    Integer totalItems;
+    Date date;
+    //User user
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(Integer totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
