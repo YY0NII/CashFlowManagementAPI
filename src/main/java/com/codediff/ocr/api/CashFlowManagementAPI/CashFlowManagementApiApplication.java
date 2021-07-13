3d79c4e4b4c63d88103b197da523b482f2759b75
Jonathon <Jonatan9102@gmail.com>
2021-07-13 09:20:50 -0400

    Initial commit

---
+++ src/main/java/com/codediff/ocr/api/CashFlowManagementAPI/CashFlowManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.codediff.ocr.api.CashFlowManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codediff/ocr/api/CashFlowManagementAPI/CashFlowManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codediff.ocr.api.CashFlowManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
