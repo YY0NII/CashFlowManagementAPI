cf71a4f78ba79cfa7d5fd20f02af5d5600c18726
Jonathon <Jonatan9102@gmail.com>
2021-07-14 09:51:46 -0400

    Commented out UserController Class due to errors

--- src/main/java/com/codediff/ocr/api/CashFlowManagementAPI/controllers/UserController.java
+++ src/main/java/com/codediff/ocr/api/CashFlowManagementAPI/controllers/UserController.java
@@ -15,64 +15,59 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    User currentUser;
-
-    @Autowired
-    UserReceiptService userReceiptService;
-
-    @PostMapping("/login")
-    public ResponseEntity<String> newUser(@RequestBody User user, HttpServletResponse response)throws IOException{
-        if (userService.getAllUsers().contains(user)){
-            return new ResponseEntity<String>("User "+user.getUserName() + "already exist", HttpStatus.IM_USED);
-        }
-        userService.createUser(user.getUserName());
-        currentUser = userService.getUser(user);
-        response.sendRedirect("/account");
-        return new ResponseEntity<String>("Welcome "+ user.getUserName() +"!", HttpStatus.CREATED);
-    }
-
-    @GetMapping
-    public ResponseEntity<String> login(User user, HttpServletResponse response)throws IOException{
-        if(userService.getAllUsers().contains(user)){
-            response.sendRedirect("/account");
-            return new ResponseEntity<String>("Welcome "+ user.getUserName() +"!", HttpStatus.CREATED);
-        }
-        else{
-            return  new ResponseEntity<>(user.getUserName() +"Not Found!", HttpStatus.NOT_FOUND);
-        }
-    }
-
-
-
-
-    @GetMapping("/Users")
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
-
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id){
-        return userService.getUserById(id);
-    }
-
-    @PostMapping("addReceipt")
-    public void addReceiptToUser(Long id, Receipt receipt){
-        User currentUser = getUserById(id);
-        currentUser.getReceipts().add(receipt);
-    }
-
-    @PostMapping("removeReceipt")
-    public void removeReceiptFromUser(Long id, Receipt receipt){
-        User currentUser = getUserById(id);
-            currentUser.getReceipts().remove(receipt);
-    }
-
-
-
-
-
+//    @Autowired
+//    UserService userService;
+//
+//    @Autowired
+//    User currentUser;
+//
+//    @Autowired
+//    UserReceiptService userReceiptService;
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<String> newUser(@RequestBody User user, HttpServletResponse response)throws IOException{
+//        if (userService.getAllUsers().contains(user)){
+//            return new ResponseEntity<String>("User "+user.getUserName() + "already exist", HttpStatus.IM_USED);
+//        }
+//        userService.createUser(user.getUserName());
+//        currentUser = userService.getUser(user);
+//        response.sendRedirect("/account");
+//        return new ResponseEntity<String>("Welcome "+ user.getUserName() +"!", HttpStatus.CREATED);
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<String> login(User user, HttpServletResponse response)throws IOException{
+//        if(userService.getAllUsers().contains(user)){
+//            response.sendRedirect("/account");
+//            return new ResponseEntity<String>("Welcome "+ user.getUserName() +"!", HttpStatus.CREATED);
+//        }
+//        else{
+//            return  new ResponseEntity<>(user.getUserName() +"Not Found!", HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//
+//
+//
+//    @GetMapping("/Users")
+//    public List<User> getAllUsers(){
+//        return userService.getAllUsers();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public User getUserById(@PathVariable Long id){
+//        return userService.getUserById(id);
+//    }
+//
+//    @PostMapping("addReceipt")
+//    public void addReceiptToUser(Long id, Receipt receipt){
+//        User currentUser = getUserById(id);
+//        currentUser.getReceipts().add(receipt);
+//    }
+//
+//    @PostMapping("removeReceipt")
+//    public void removeReceiptFromUser(Long id, Receipt receipt){
+//        User currentUser = getUserById(id);
+//            currentUser.getReceipts().remove(receipt);
+//    }
 }
