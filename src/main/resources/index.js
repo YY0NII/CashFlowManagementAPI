cd9d3eba389a9adaa2696ca0b92f65be506d8d17
Kyle <kmcallister317@gmail.com>
2021-07-15 10:29:14 -0400

    front end with login function

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -16,11 +16,18 @@ function createNewUser(e){
 
 }
 
-function login(e){
-    e.preventDefault();
+function login(form){
+    /*e.preventDefault();
     let user = {
         userName: document.getElementById("Username").value,
         password: document.getElementById("password").value,
+    }*/
+    if((form.username.value == "test") && (form.password.value == "test")) {
+        self.location.href = "index.html";
+    }
+    else {
+        alert("username or password is wrong");
+        return false;
     }
 
     fetch("http://localhost:8080/login",
@@ -61,8 +68,8 @@ function deleteAll(e){
 async function getAllReceipts(){
     let response = await fetch("http://localhost:8080/users");
     let body = await response.json();
-    let receipt = body.map(receipts => {
-    let date = new Date(receipts.date)
+    let receipt = body.map(receipt => {
+    let date = new Date(receipt.date)
         return (
             `<li class="list-group-item receipt">
                 <p>${date.getMonth()+1}/${date.getDate()+1}/${date.getFullYear()}</p>
@@ -72,6 +79,6 @@ async function getAllReceipts(){
             </li>`
         );
     }).join("");
-    document.getElementById("receipts").innerHTML = receipts;
+//    document.getElementById("receiptPicture").innerHTML;
 }
 getAllReceipts();
