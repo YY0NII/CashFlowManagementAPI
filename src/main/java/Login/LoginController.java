6c5b7b0cb9aa20ff95887ec16dbc4e3bf59442ba
Nigel Jennings <nigel.jennings2001@gmail.com>
2021-07-15 17:01:55 -0400

    added a new feature to the login.html

---
+++ src/main/java/Login/LoginController.java
@@ -0,0 +1,20 @@
+package Login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+    import org.springframework.stereotype.Controller;
+   import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+    public class LoginController {
+
+        @GetMapping("/login")
+        public String login() {
+            return "login";
+        }
+
+    }
+
+
